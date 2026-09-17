2a394cea07b9935327d8264cff59f7fe5207e05f
kkvlnv <kaylavillanuevaalt@gmail.com>
2026-09-17 19:47:49 +0800

    Update q1_sg7_pinatubo_villanueva.py

--- q1/q1_sg7_pinatubo_villanueva.py
+++ q1/q1_sg7_pinatubo_villanueva.py
@@ -1,2 +1,29 @@
 class Glassware:
-  def __init__(self,
+  def __init__(self):
+    print("Glassware created")
+  def __del__(self):
+    print("Glassware is gone")
+
+class Beaker(Glassware):
+  def __init__(self):
+    super().__init__()
+    print("Beaker created")
+  def __del__(self):
+    print("Beaker is missing")
+
+class Tray:
+  def __init__(self):
+    print("Tray created")
+    self.beakers = [
+       Beaker(),
+       Beaker(),
+       Beaker(),
+       Beaker(),
+       Beaker()
+    ]
+  def __del__(self):
+    del self.beakers
+    print("Tray is missing")
+
+tray = Tray()
+del tray
