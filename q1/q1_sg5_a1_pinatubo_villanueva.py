505c9737f13b3e49c0cc295c02425ed179c70e24
kkvlnv <kaylavillanuevaalt@gmail.com>
2026-09-18 07:58:19 +0800

    Update q1_sg5_a1_pinatubo_villanueva.py

--- q1/q1_sg5_a1_pinatubo_villanueva.py
+++ q1/q1_sg5_a1_pinatubo_villanueva.py
@@ -1,1 +1,13 @@
+class Hero:
+  def __init__(self, name, hp):
+    self.name = name
+    self.hp = hp
 
+  def hurt(self, amount):
+    self.hp = self.hp - amount
+
+arthur = Hero("Arthur", 100)
+morgana = Hero("Morgana", 100)
+arthur.hurt(10)
+
+print(f"{arthur.name} HP: {arthur.hp}\n{morgana.name} HP: {morgana.hp}")
